0fe8eabe9af2755291fe60cd3dd1ddbbcd1a7ffa
Nithingowtham129 <132273083+Nithingowtham129@users.noreply.github.com>
2024-09-03 14:41:49 +0530

    Add files via upload

---
+++ Leaf Disease Detection/Source_file/Train_file.py
@@ -0,0 +1,58 @@
+# Leaf diesease Detection and classification
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+
+# cnn
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape = (128, 128, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(96, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(13, activation='softmax'))
+
+model.compile(optimizer= 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+train_datagen = ImageDataGenerator(rescale = None, shear_range = 0.2,  zoom_range = 0.2, horizontal_flip = True)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('C:/Users/NITHIN GOWTHAM/Documents/java opp/Python/leaf dataset/Train', target_size = (128, 128), batch_size = 32, class_mode = 'categorical')
+
+labels = (training_set.class_indices)
+print(labels)
+
+test_set = test_datagen.flow_from_directory('C:/Users/NITHIN GOWTHAM/Documents/java opp/Python/leaf dataset/Test', target_size = (128, 128), batch_size = 32, class_mode = 'categorical')
+
+label2 = (test_set.class_indices)
+print(label2)
+
+model.fit_generator(training_set, steps_per_epoch = 375, epochs = 10, validation_data = test_set, validation_steps =125)
+
+# Model Save
+# model_json = model.to_json()
+# with open("model.json", "w") as json_file:
+#     json_file.write(model_json)
+# model.save_weights("model.h5")
+# print("Saved model to Disc")
